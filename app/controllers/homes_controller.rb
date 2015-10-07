ff7dbba5fcddf0ae2d69c936f4bb25a8c308208d
nathanielltaylor <ntaylor390@gmail.com>
2015-10-07 12:35:47 -0400

    hardcoded marker working

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,9 @@
 class HomesController < ApplicationController
   def index
+    ip_address = request.remote_ip unless Rails.env.test? || Rails.env.development?
+    ip_address = "50.241.127.209" if Rails.env.test? || Rails.env.development?
+    location = GeoIP.new('GeoLiteCity.dat').city(ip_address)
+    @lat = location.latitude
+    @lng = location.longitude
   end
 end
