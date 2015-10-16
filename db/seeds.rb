e029e193d90624378dcbcadb029b2fd4350c30b5
nathanielltaylor <ntaylor390@gmail.com>
2015-10-16 18:56:24 -0400

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+bob = User.create(email: "test@test.com", username: "big bob", password: "test")
+bill = User.create(email: "test@test.com", username: "billy", password: "test")
+sam = User.create(email: "test@test.com", username: "samuel", password: "test")
+mary = User.create(email: "test@test.com", username: "mary", password: "test")
+
+Search.create(query: "Boston", trend_or_location: "Location", user: bob)
+Search.create(query: "Miami", trend_or_location: "Location", user: mary)
+Search.create(query: "Buenos Aires", trend_or_location: "Location", user: bill)
+Search.create(query: "debate", trend_or_location: "Trend", user: sam)
+Search.create(query: "trump", trend_or_location: "Trend", user: bob)
+Search.create(query: "HIV", trend_or_location: "Trend", user: mary)
+Search.create(query: "beyonce", trend_or_location: "Trend", user: bob)
+
+Recommendation.create(query: "trump", trend_or_location: "Trend", category: "Politics and Religion", user: bob)
+Recommendation.create(query: "love", trend_or_location: "Trend", category: "General", user: sam)
+Recommendation.create(query: "hoverboard", trend_or_location: "Trend", category: "Science and Technology", user: mary)
+Recommendation.create(query: "Pyongyang", trend_or_location: "Location", category: "Politics and Religion", user: bill)
